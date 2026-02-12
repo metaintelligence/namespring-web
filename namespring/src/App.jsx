418a1fb5e026339e9f8cf1599f39a9eede289007
Artrointel <artrointel@gmail.com>
2026-02-12 22:01:32 +0900

    Setup Vite + React App env.

---
+++ namespring/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold text-blue-500 drop-shadow-md">
+        이름봄(NameSpring) POC 시작
+      </h1>
+    </div>
+  );
+}
+export default App;
